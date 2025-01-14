d144e1cbc26142d3b1ea24646987bbbcb7a672ec
ummesalma303 <ummesalma333999@gmail.com>
2025-01-14 13:30:54 +0600

    adde user post api

--- index.js
+++ index.js
@@ -28,9 +28,20 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+    const userCollection = client.db("SwiftParcel").collection("users");
+
+    app.post('/users',async(req,res)=>{
+      const users = req.body
+      const result = await userCollection.insertOne(users)
+      console.log(result)
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
